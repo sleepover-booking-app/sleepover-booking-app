991a1131f593891cfbffdb8a7ba8f84ed1f9068d
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-19 16:22:25 +0200

    add ignore file

--- src/util.js
+++ src/util.js
@@ -33,12 +33,16 @@ export function encodeObject(object) {
     return encodeURIComponent(JSON.stringify(object));
 }
 
+export function encodeDate(date) {
+    return { __type: 'Date', iso: date.toISOString() };
+}
+
 export function submitHandler(callback) {
     return function (event) {
         event.preventDefault();
         const formData = new FormData(event.target);
         const data = Object.fromEntries([...formData].map(([k, v]) => [k, v.trim()]));
-    
+
         callback(data, event.target);
     };
 }
